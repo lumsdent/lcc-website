1207985c0cb64489e52d36e0f91b72d3e3a44ebb
lumsdent <tyler.j.lumsden@gmail.com>
2024-06-16 11:11:23 -0400

    move more css to tailwind

--- src/App.vue
+++ src/App.vue
@@ -11,39 +11,57 @@ import AppHeader from './components/AppHeader.vue'
       <AppHeader msg="Welcome to LCC!" />
 
       <nav class="-ml-4 mt-4 py-4 text-left text-base">
-        <RouterLink class="inline-block border-l px-4 first-of-type:border-0" to="/">Home</RouterLink>
-        <RouterLink class="inline-block border-l px-4" to="/match">Match Input</RouterLink>
-        <RouterLink class="inline-block border-l px-4" to="/registration">Registration</RouterLink>
-        <RouterLink class="inline-block border-l px-4" to="/draft">Draft</RouterLink>
-        <RouterLink class="inline-block border-l px-4" to="/practice">Practice</RouterLink>
-        <RouterLink class="inline-block border-l px-4" to="/stats">Stats</RouterLink>
-        <RouterLink class="inline-block border-l px-4" to="/schedule">Schedule</RouterLink>
-        <RouterLink class="inline-block border-l px-4" to="/contact">Contact Us</RouterLink>
+        <RouterLink
+          class="inline-block border-l px-4 first-of-type:border-0"
+          activeClass="hover:bg-transparent text-text-dark"
+          to="/"
+          >Home</RouterLink
+        >
+        <RouterLink
+          class="inline-block border-l px-4"
+          activeClass="hover:bg-transparent text-text-dark"
+          to="/match"
+          >Match Input</RouterLink
+        >
+        <RouterLink
+          class="inline-block border-l px-4"
+          activeClass="hover:bg-transparent text-text-dark"
+          to="/registration"
+          >Registration</RouterLink
+        >
+        <RouterLink
+          class="inline-block border-l px-4"
+          activeClass="hover:bg-transparent text-text-dark"
+          to="/draft"
+          >Draft</RouterLink
+        >
+        <RouterLink
+          class="inline-block border-l px-4"
+          activeClass="hover:bg-transparent text-text-dark"
+          to="/practice"
+          >Practice</RouterLink
+        >
+        <RouterLink
+          class="inline-block border-l px-4"
+          activeClass="hover:bg-transparent text-text-dark"
+          to="/stats"
+          >Stats</RouterLink
+        >
+        <RouterLink
+          class="inline-block border-l px-4"
+          activeClass="hover:bg-transparent text-text-dark"
+          to="/schedule"
+          >Schedule</RouterLink
+        >
+        <RouterLink
+          class="inline-block border-l px-4"
+          activeClass="hover:bg-transparent text-text-dark"
+          to="/contact"
+          >Contact Us</RouterLink
+        >
       </nav>
     </div>
   </header>
 
   <RouterView />
 </template>
-
-<style scoped>
-
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-/* nav a:first-of-type {
-  border: 0;
-} */
-</style>
